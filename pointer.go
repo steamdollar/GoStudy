bd9d3ef5541f10c09a7d7d565f34b67a91ed9291
이승준 <chronica@iseungjun-ui-MacBookAir.local>
2022-12-04 12:38:23 +0900

    minor fix

--- pointer.go
+++ pointer.go
@@ -2,16 +2,17 @@
 // &a는 변수 a의 memory address를 출력해준다.
 
 package main
+
 import "fmt"
 
 func main() {
-	a := 20;
+	a := 20
 	fmt.Println("Address : ", &a)
 	// a의 value는 메모리의 어디에 저장되어있는지를 숫자로 표현
 	fmt.Println("Value:", a)
 
 	b := &a
-	fmt.Println("b :",  b)
+	fmt.Println("b :", b)
 	fmt.Println("*b :", *b)
 	// b라는 a의 address에는 어떤값이 있는지를 알려줌
 
